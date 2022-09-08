cd4a41a8b7617c6511f35e9f5d1ce15e4c6c2157
Nick <dibartolonick0518@gmail.com>
2022-09-08 13:15:25 -0400

    edits the server.js file

--- server.js
+++ server.js
@@ -1,4 +1,9 @@
 const express = require('express');
 
 const app = express();
-const PORT = 3001;
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, () => {
+    console.log(`PORT: ${PORT} is being listened to by the server`);
+  });
