b4b0f14ee55f4e14dbd6024d3d1e49e26b836f0c
Nick <dibartolonick0518@gmail.com>
2022-09-09 16:14:08 -0400

    moved all code from mainroute.js to server.js, organized folders

--- server.js
+++ server.js
@@ -1,12 +1,32 @@
 const express = require('express');
-
+const path = require('path');
 const app = express();
 const PORT = process.env.PORT || 3000;
+// user router
+const uRouter = require("./")
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.use(express.static('public'));
+app.use('/api', uRouter);
+
+
+app.get('/', (req, res)=> 
+res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
+// GET route for notes page
+app.get('/notes', (req,res) => 
+res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+
+// Reroute to home if route does not exist
+app.get('*', (req,res) =>
+res.sendFile(path.join(__dirname, './public/index.html'))
+);
+
 
 
 
