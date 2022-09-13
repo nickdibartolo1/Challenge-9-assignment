352d7471052bb3d4f08442832d43fdd5089a07dc
Nick <dibartolonick0518@gmail.com>
2022-09-13 12:43:13 -0400

    edits in mainRoute and creation of notes.js file

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -1,16 +1,25 @@
 const path = require('path');
+const express = require('express');
+const app = express();
+const notesRouter = require('./notes')
 
-module.exports = (app) => {
-   
-    app.get('/notes', (req, res) => {
-      res.sendFile(path.join(__dirname, '../public/notes.html'));
-    });
-  
-    app.get('/', (req, res) => {
-      res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-  
-    app.get('*', (req, res) => {
-      res.sendFile(path.join(__dirname, '../public/index.html'));
-    });
-  };
+
+
+app.use('/notes', notesRouter)
+
+
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/notes.html'));
+});
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = app;
+
+//   console.log(app)
