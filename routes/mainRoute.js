9272e6e864a6edd85d0118ab635b566b0111c3b1
Nick <dibartolonick0518@gmail.com>
2022-09-18 21:19:06 -0400

    small edits to files

--- routes/mainRoute.js
+++ routes/mainRoute.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/notes.html'));
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
